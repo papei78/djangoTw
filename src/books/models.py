8ac81b31d6a144726ce2f243a09deb8cc51f6c51
aemon <khazriaymen93@gmail.com>
2024-04-27 00:05:57 +0200

    images and qrcode generation

--- src/books/models.py
+++ src/books/models.py
@@ -3,6 +3,11 @@ from publishers.models import Publisher
 from authors.models import Author
 from django.utils.text import slugify
 import uuid
+#imports for qrcode generation
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image
 
 # Create your models here.
 
@@ -29,7 +34,7 @@ class BookTitle(models.Model):
 class Book(models.Model):
     book_id = models.CharField(max_length=24, blank=True)
     title= models.ForeignKey(BookTitle, on_delete=models.CASCADE)
-    # qr_code
+    qr_code = models.ImageField(upload_to='qr_codes', blank=True, null=True)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
 
@@ -39,6 +44,15 @@ class Book(models.Model):
     def save(self, *args, **kwargs):
         if not self.book_id:
             self.book_id = str(uuid.uuid4()).replace('-','')[:24].lower()
+        qrcode_img = qrcode.make(self.book_id)
+        canvas = Image.new('RGB', (qrcode_img.pixel_size, qrcode_img.pixel_size), 'white')
+        canvas.paste(qrcode_img)
+        fname=f'qr_code-{self.title}.png'
+        buffer = BytesIO()
+        canvas.save(buffer, 'PNG')
+        self.qr_code.save(fname, File(buffer), save=False)
+        canvas.close()
+
         super().save(*args, **kwargs)
 
     
