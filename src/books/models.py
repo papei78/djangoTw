3415861169cbd785dca8f31d9223066f9cb413b3
aemon <khazriaymen93@gmail.com>
2024-04-26 18:02:30 +0200

    novice models' things

---
+++ src/books/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from publishers.models import Publisher
+from authors.models import Author
+from django.utils.text import slugify
+import uuid
+
+# Create your models here.
+
+class BookTitle(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    #optional field
+    slug=models.SlugField(blank=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"Book position: {self.title}"
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug=slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+
+class Book(models.Model):
+    book_id = models.CharField(max_length=24, blank=True)
+    title= models.ForeignKey(BookTitle, on_delete=models.CASCADE)
+    # qr_code
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    def __str__(self) :
+        return str(self.title)
+
+    def save(self, *args, **kwargs):
+        if not self.book_id:
+            self.book_id = str(uuid.uuid4()).replace('-','')[:24].lower()
+        super().save(*args, **kwargs)
+
+    
