9d7ca5b92e0297551084bd9d69667b447adca0a6
aemon <khazriaymen93@gmail.com>
2024-05-12 19:39:54 +0200

    add a header and go back btn

--- src/static/main.js
+++ src/static/main.js
@@ -1,1 +1,12 @@
-console.log('hello xorld')
+const head = document.getElementById('head')
+
+
+
+if(head){
+
+    const goBackBtn = document.getElementById('go-back-btn')
+    // the question mark is to verify that the variable is not null nor undefined
+    goBackBtn?.addEventListener('click',()=>{
+        history.back()
+    })
+}
