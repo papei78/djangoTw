7064299a85b2aef2893f62a40c0c0bb8e47622e4
aemon <khazriaymen93@gmail.com>
2024-05-29 18:12:39 +0200

    search by isbn and id

--- src/rentals/static/rentals/main.js
+++ src/rentals/static/rentals/main.js
@@ -2,12 +2,13 @@ const form = document.getElementById('search-form')
 const input = document.getElementById('id_search')
 
 const BOOK_ID_LENGTH =36
+const ISBN_LENGTH =36
 
 input.focus()
 
 
 input.addEventListener('keyup',()=>{
-    if (input.value.length === BOOK_ID_LENGTH){
+    if (input.value.length === BOOK_ID_LENGTH || input.value.length === ISBN_LENGTH){
         form.submit()
     } 
 })
--- src/rentals/views.py
+++ src/rentals/views.py
@@ -3,11 +3,11 @@ from .forms import SearchBookForm
 from books.models import Book
 from django.views.generic import ListView
 from .models import Rental
-
+from django.db.models import Q
 def search_book_view(request):
     form = SearchBookForm(request.POST or None)
     search_query = request.POST.get('search',None)
-    book_ex = Book.objects.filter(id=search_query).exists()
+    book_ex = Book.objects.filter(Q(isbn=search_query)| Q(id=search_query)).exists()
 
 
     if search_query is not None and book_ex:
@@ -25,12 +25,12 @@ class BookRentalHistoryView(ListView):
 
     def get_queryset(self):
         book_id = self.kwargs.get('book_id')
-        return Rental.objects.filter(book__id=book_id)
+        return Rental.objects.filter(Q(book__isbn=book_id)| Q(book__id=book_id))
     
     def get_context_data(self, **kwargs):
         context=super().get_context_data(**kwargs)
         book_id = self.kwargs.get('book_id')
-        # obj = Book.objects.filter(id=book_id).first()
-        obj = get_object_or_404(Book, id=book_id)
+        obj = Book.objects.filter(Q(isbn=book_id)| Q(id=book_id)).first()
+        # obj = get_object_or_404(Book, Q(isbn=book_id)| Q(id=book_id))
         context['object'] = obj
         return context
